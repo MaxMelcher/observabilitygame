a6f0d92de19817a22fddbf1223f7cf5fec897939
Max Melcher <Maximilian.Melcher@melcher.it>
2025-04-08 23:51:18 +0200

    50 chars max

--- backend/Models/PlayerScore.cs
+++ backend/Models/PlayerScore.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace backend.Models;
@@ -5,6 +6,7 @@ namespace backend.Models;
 public class PlayerScore
 {
     public int Id { get; set; }
+    [MaxLength(50)]
     public string PlayerName { get; set; } = string.Empty;
     public double Time { get; set; }  // time in milliseconds
     public DateTime Created { get; set; }
