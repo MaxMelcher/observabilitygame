35b02c054d1cac7b0ba0dddcb6cf74db72897374
Max Melcher <Maximilian.Melcher@melcher.it>
2025-04-08 12:07:22 +0200

    app insights

--- frontend/src/app/services/app-insights.service.ts
+++ frontend/src/app/services/app-insights.service.ts
@@ -14,7 +14,7 @@ export class AppInsightsService {
     this.angularPlugin = new AngularPlugin();
     this.appInsights = new ApplicationInsights({
       config: {
-        connectionString: '',
+        connectionString: 'InstrumentationKey=be2e2c9c-780e-4ebf-899e-9ae69b3edede;IngestionEndpoint=https://germanywestcentral-1.in.applicationinsights.azure.com/;LiveEndpoint=https://germanywestcentral.livediagnostics.monitor.azure.com/;ApplicationId=e4e28e1c-e942-40ea-a0a5-277e33e6d646',
         enableAutoRouteTracking: true,
         extensions: [this.angularPlugin],
         extensionConfig: {
